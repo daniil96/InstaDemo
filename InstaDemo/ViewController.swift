0b4e1005a5d73014046fb9fc7090e91d794999e2
Kornev Daniil <Daniilkornev96@mail.ru>
2024-11-01 21:59:07 +0500

    Initial Commit

---
+++ InstaDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaDemo
+//
+//  Created by A1 on 01.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
