c45ad4cb5319587aa32dd60adc66226a1c9fe78f
Kornev Daniil <Daniilkornev96@mail.ru>
2024-11-01 22:33:18 +0500

    Setup project

--- InstaDemo/Screens/Profile/ProfileViewController.swift
+++ InstaDemo/Screens/Profile/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  InstaDemo
 //
 //  Created by A1 on 01.11.2024.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
-
-
 }
 
